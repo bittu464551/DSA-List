889ee6671dfa73f96355f184ca32513ec335dde1
Kamlendra Gour <76568890+bittu464551@users.noreply.github.com>
2022-07-17 07:51:55 +0530

    Update Hello_world.java

--- Hello_world.java
+++ Hello_world.java
@@ -3,5 +3,6 @@ package javase;
 public class Hello_world {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Goog Wishes for You");
     }
 }
