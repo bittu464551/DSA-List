ef1804b8702504b0c483bab3ae8ca71df1d20302
Kamlendra Gour <76568890+bittu464551@users.noreply.github.com>
2022-07-27 08:59:51 +0530

    Add files via upload

---
+++ FindPairWithSumSortedRotatedArray.java
@@ -0,0 +1,31 @@
+package Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class FindPairWithSumSortedRotatedArray {
+    public boolean pairWithSum(int arr[],int num){
+        int n=arr.length;
+        ArrayList<Integer> al=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            int temp=num-arr[i];
+            if(al.contains(temp)){
+                return true;
+            }
+            else{
+                al.add(arr[i]);
+            }
+            
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        FindPairWithSumSortedRotatedArray obj=new FindPairWithSumSortedRotatedArray();
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int nums[]=new int[num];
+        for(int i=0;i<num;i++){
+            nums[i]=sc.nextInt();
+        }
+        int find=sc.nextInt();
+        System.out.println(obj.pairWithSum(nums,find));
+    }
+}
